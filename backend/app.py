a6f8fae3819fcc44efe0ee9f3357b54d6409b224
zbrwkq <damien.chv78@gmail.com>
2024-10-09 15:14:30 +0200

    producer

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,32 @@
+from kafka import KafkaProducer
+from time import sleep
+import json
+import random
+from datetime import datetime
 import random
 import time
 import json
 
-# Fonction pour générer des données d'astéroïdes simulées
-def generate_asteroid_data():
-    # Caractéristiques simulées de l'astéroïde
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+id = 0
+
+def generate_data():
     asteroid_data = {
-        "asteroid_id": random.randint(1000, 9999),
+        "asteroid_id": id,
         "size": round(random.uniform(10.0, 1000.0), 2),  
         "velocity": round(random.uniform(5.0, 50.0), 2), 
         "distance_from_earth": round(random.uniform(0.1, 10.0), 4),  
         "orbital_period": round(random.uniform(0.5, 3.0), 2),  
         "direction_angle": round(random.uniform(0.0, 360.0), 2),  
     }
+    id += 1
     return asteroid_data
 
-# Fonction pour générer des données en continu
-def simulate_asteroid_data_stream():
-    while True:
-        data = generate_asteroid_data()
-        print(json.dumps(data))  # Print les données générées au format JSON
-        time.sleep(1)  # Pause de 1 seconde entre chaque génération de données
+while True:
+    data = generate_data()
+    producer.send('data-stream', data)
+    print(f"Sent data: {data}")
+    sleep(2)
 
-# Exécuter la simulation
-if __name__ == "__main__":
-    simulate_asteroid_data_stream()
+
