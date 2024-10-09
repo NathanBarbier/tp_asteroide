a15a5c2f6d830f4dd3db85aca6928bc23e10f1ad
zbrwkq <damien.chv78@gmail.com>
2024-10-09 16:58:32 +0200

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,39 +1,55 @@
+from flask import Flask, request, jsonify
 from kafka import KafkaProducer
-from time import sleep
+from marshmallow import Schema, fields, ValidationError
 import json
 import random
-import random
-import time
-import json
 
+app = Flask(__name__)
 
 producer = KafkaProducer(
     bootstrap_servers='kafka:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-topic_name = 'topic1'
+class AsteroidSchema(Schema):
+    count = fields.Int(required=True)
+
+asteroid_schema = AsteroidSchema()
 
 id = 0
 
-def generate_data():
+def generate_asteroid():
+    global id
     asteroid_data = {
         "asteroid_id": id,
         "size": round(random.uniform(10.0, 1000.0), 2),  
         "velocity": round(random.uniform(5.0, 50.0), 2), 
-        "direction_angle": {"x": round(random.uniform(0.0, 1000000.0), 2), "y": round(random.uniform(0.0, 1000000.0), 2), "z": round(random.uniform(0.0, 1000000.0), 2)},  
-        "pos": {"x": round(random.uniform(0.0, 1000000.0), 2), "y": round(random.uniform(0.0, 1000000.0), 2), "z": round(random.uniform(0.0, 1000000.0), 2)},
+        "direction": {"x": round(random.uniform(0.0, 1000000.0), 2), "y": round(random.uniform(0.0, 1000000.0), 2), "z": round(random.uniform(0.0, 1000000.0), 2)},  
+        "position": {"x": round(random.uniform(0.0, 1000000.0), 2), "y": round(random.uniform(0.0, 1000000.0), 2), "z": round(random.uniform(0.0, 1000000.0), 2)},
     }
     id += 1
     return asteroid_data
 
-try:
-    while True:
-        data = generate_data()
-        producer.send(topic_name, value=data)
-        print(f"Sent: {data}")
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Stopping producer...")
+@app.route('/asteroid', methods=['POST'])
+def add_user():
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({"message": "No input data provided"}), 400
 
-producer.close()
+    try:
+        data = asteroid_schema.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 422
+
+    asteroids = []
+    for i in range(0, data['count'], 1):
+        asteroids.append(generate_asteroid())
+
+    
+    producer.send('topic1', asteroids)
+    producer.flush()
+
+    return jsonify({"message": "Asteroid data received and sent to Kafka"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5550)
