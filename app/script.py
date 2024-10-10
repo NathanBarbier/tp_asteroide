fcd334e7be2b29e67df9e36684f1abecd21814dd
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2024-10-10 14:47:28 +0200

    consumer intercatif que produc aussi (#9)

--- app/script.py
+++ app/script.py
@@ -4,62 +4,78 @@ from pyspark.sql.functions import col, max as spark_max
 import math
 from datetime import datetime
 
+def update_asteroids(a,b):
+    spark = SparkSession.builder.appName("MyApp").getOrCreate()
 
-spark = SparkSession.builder.appName("MyApp").getOrCreate()
+    old_df = None
+    new_df = None
 
-new_df = spark.read.json("hdfs://namenode:9000/Data/asteroid_data/")
-old_df = spark.read.json("hdfs://namenode:9000/Data/updated_asteroid_data/")
+    try:
+        new_df = spark.read.json("hdfs://namenode:9000/Data/asteroid_data/")
+    except Exception as e:
+        print(e)
 
-df = new_df.union(old_df)
+    try:
+        old_df = spark.read.json("hdfs://namenode:9000/Data/updated_asteroid_data/")
+    except Exception as e:
+        print(e)
 
-latest_updates = df.groupBy("asteroid_id").agg(spark_max("updated_at").alias("updated_at"))
+    if old_df is not None and new_df is not None:
+        df = new_df.union(old_df)
+    elif old_df is not None and new_df is None:
+        df = old_df
+    elif old_df is None and new_df is not None:
+        df = new_df
+    else:
+        return
 
-df = df.join(latest_updates, ["asteroid_id", "updated_at"])
+    latest_updates = df.groupBy("asteroid_id").agg(spark_max("updated_at").alias("updated_at"))
 
-
-def update_position(asteroid_row):
-    asteroid = asteroid_row.asDict()
-
-    direction_magnitude = math.sqrt(
-        asteroid["direction"]["x"] ** 2 + 
-        asteroid["direction"]["y"] ** 2 + 
-        asteroid["direction"]["z"] ** 2
-    )
-
-    normalized_direction = {
-        "x": asteroid["direction"]["x"] / direction_magnitude,
-        "y": asteroid["direction"]["y"] / direction_magnitude,
-        "z": asteroid["direction"]["z"] / direction_magnitude
-    }
-
-    new_position = {
-        "x": asteroid["position"]["x"] + normalized_direction["x"] * asteroid["velocity"],
-        "y": asteroid["position"]["y"] + normalized_direction["y"] * asteroid["velocity"],
-        "z": asteroid["position"]["z"] + normalized_direction["z"] * asteroid["velocity"]
-    }
-
-    return {
-        "asteroid_id": asteroid["asteroid_id"],
-        "size": asteroid["size"],
-        "velocity": asteroid["velocity"],
-        "direction": asteroid["direction"],
-        "position": new_position,
-        "updated_at":datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    }
-
-updated_asteroids = df.rdd.map(lambda row: update_position(row)).collect()
-
-updated_df = spark.createDataFrame(updated_asteroids)
-
-updated_df.show(truncate=False)
-
-updated_df.write.mode("overwrite").json("hdfs://namenode:9000/Data/updated_asteroid_data/")
+    df = df.join(latest_updates, ["asteroid_id", "updated_at"])
 
 
-# asteroid_data = {
-#     "asteroid_id": id,
-#     "size": round(random.uniform(10.0, 1000.0), 2),  
-#     "velocity": round(random.uniform(5.0, 50.0), 2), 
-#     "direction_angle": {"x": round(random.uniform(0.0, 1000000.0), 2), "y": round(random.uniform(0.0, 1000000.0), 2), "z": round(random.uniform(0.0, 1000000.0), 2)},  
-#     "pos": {"x": round(random.uniform(0.0, 1000000.0), 2), "y": round(random.uniform(0.0, 1000000.0), 2), "z": round(random.uniform(0.0, 1000000.0), 2)},
-# }
+    def update_position(asteroid_row):
+        asteroid = asteroid_row.asDict()
+
+        direction_magnitude = math.sqrt(
+            asteroid["direction"]["x"] ** 2 + 
+            asteroid["direction"]["y"] ** 2 + 
+            asteroid["direction"]["z"] ** 2
+        )
+
+        normalized_direction = {
+            "x": asteroid["direction"]["x"] / direction_magnitude,
+            "y": asteroid["direction"]["y"] / direction_magnitude,
+            "z": asteroid["direction"]["z"] / direction_magnitude
+        }
+
+        new_position = {
+            "x": asteroid["position"]["x"] + normalized_direction["x"] * asteroid["velocity"],
+            "y": asteroid["position"]["y"] + normalized_direction["y"] * asteroid["velocity"],
+            "z": asteroid["position"]["z"] + normalized_direction["z"] * asteroid["velocity"]
+        }
+
+        return {
+            "asteroid_id": asteroid["asteroid_id"],
+            "size": asteroid["size"],
+            "velocity": asteroid["velocity"],
+            "direction": asteroid["direction"],
+            "position": new_position,
+            "updated_at":datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+    updated_asteroids = df.rdd.map(lambda row: update_position(row)).collect()
+
+    updated_df = spark.createDataFrame(updated_asteroids)
+
+    updated_df.show(truncate=False)
+
+    df_to_send = df.selectExpr("CAST(asteroid_id AS STRING)", "to_json(struct(*)) AS value")
+
+    df_to_send.write \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:9092") \
+        .option("topic", "topic3") \
+        .save()
+
+    updated_df.write.mode("overwrite").json("hdfs://namenode:9000/Data/updated_asteroid_data/")
